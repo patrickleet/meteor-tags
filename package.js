909efb275c640ef38ef70e39df3a9b119e453728
Patrick Scott <patrick.lee.scott@gmail.com>
2014-07-30 11:48:30 -0400

    remove client deps

--- package.js
+++ package.js
@@ -4,8 +4,6 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-    api.use(['underscore', 'bootstrap', 'templating', 'jquery', 'handlebars'], 'client');
-    
     // this is required since Meteor 0.6.5
     api.use(['livedata', 'mongo-livedata'], ['client', 'server']);
 
