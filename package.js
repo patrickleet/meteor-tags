2060afaaaebdba8682e832f0dea5e25856df47c7
Patrick Scott <patrick.lee.scott@gmail.com>
2014-07-30 11:44:04 -0400

    removed ui stuff

--- package.js
+++ package.js
@@ -11,20 +11,6 @@ Package.on_use(function (api) {
 
     api.add_files("lib/tags.js", ['client', 'server']);
 
-    api.add_files([
-
-      // HTML templates
-      'lib/listOfTags.html',
-      'lib/newTagDialog.html',
-
-      // JS source
-      'lib/listOfTags.js',
-      'lib/newTagDialog.js',
-      'lib/helpers.js',
-      'lib/tags.css',
-
-    ], 'client');
-
     if (api.export !== undefined) {
       api.export('Tags');
     }
