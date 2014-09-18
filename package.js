156011eaa19bc08eb445d1f792b03fc5c2fdfc86
Patrick Scott <patrick.lee.scott@gmail.com>
2014-09-18 18:49:19 -0400

    add support for tag groups, and weak dependency for users of collection2

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
-
 Package.describe({
-    summary: "Add tags to selected collections",
+  summary: "Add tags to selected collections and support for creating multiple groups of tags",
 });
 
 Package.on_use(function (api) {
     // this is required since Meteor 0.6.5
     api.use(['livedata', 'mongo-livedata'], ['client', 'server']);
+    api.use('aldeed:collection2', ['client', 'server'], {weak: true});
 
     api.add_files("lib/tags.js", ['client', 'server']);
 
